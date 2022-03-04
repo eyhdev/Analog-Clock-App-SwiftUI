d1a53b91ae438d06d1f53f712b808dbe8ed09aca
CodeNour <ayman.jibrael@icloud.com>
2022-03-04 14:02:05 +0300

    Initial Commit

---
+++ Clock App/Clock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_AppApp.swift
+//  Clock App
+//
+//  Created by ayhan on 4.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Clock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
